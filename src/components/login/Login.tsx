2ce0127872ee0fb1c1df6f973a31fb37dc67642f
SS007 <rbkmoneyukk@bk.ru>
2020-08-14 17:05:51 +0400

    add redux-form

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import {Field, reduxForm} from 'redux-form'
 
-const Login = (props: any) => {
-    return <h1>LOGIN</h1>
+
+
+const Login = () => {
+    const onSubmit = (formData: any) => {
+        console.log(formData)
+    }
+
+    return (
+        <div>
+            <h1>LOGIN</h1>
+            <LoginReduxForm
+                // @ts-ignore
+                onSumbit={onSubmit}/>
+        </div>
+    )
 }
+
+
+
+const LoginForm = (props: any) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={'Login'} name={'login'} component={'input'}/>
+            </div>
+            <div>
+                <Field placeholder={'Password'} name={'password'} component={'input'} type="password"/>
+            </div>
+            <div>
+                <Field component={'input'} name={'rememberMe'} type="checkbox"/> remember me
+            </div>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    )
+}
+
+// @ts-ignore
+let LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
 export default Login
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -5,16 +5,19 @@ import sidebarReducer from './sidebar-reducer'
 import usersReducer from './users_reducer'
 import authReducer from './auth_reducer'
 import thunkMiddleware from 'redux-thunk'
+import { reducer as formReducer } from 'redux-form'
 
-let reducers = combineReducers({
+
+let rootReducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware))
+let store = createStore(rootReducers, applyMiddleware(thunkMiddleware))
 
 export default store
 
